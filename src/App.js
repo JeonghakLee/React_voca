77ea090d5b5122e78ffb743f3e0aa7f4898b88b9
Jeonghak Lee <wjdgkr0527@naver.com>
2021-12-31 19:58:41 +0900

    lec 10~12: Router, Json server API, useEffect, fetch, Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import './App.css';
 import DayList from './component/DayList';
 import Header from './component/Header'
 import Day from './component/Day';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import EmptyPage from './component/EmptyPage';
 
-
+// /가 일치하기 때문에 exact를 붙여줘야 한다. v6 부턴 안해도됨
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <DayList />
-        <Day />
-    </div>
+    <BrowserRouter>    
+      <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<DayList />}></Route>
+            <Route path="/day/:day" element = {<Day />}></Route>
+            <Route element={<EmptyPage />}></Route>
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,20 +1,34 @@
 import dummy from "../db/data.json"
+import {useParams} from "react-router-dom";
+import Word from "./Word"
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
-    const day = 1;
-    const wordList = dummy.words.filter(word => 
-        word.day === day)
+    const { day } = useParams();
+    const words = useFetch(`http://localhost:3001/words?day=${day}`)
+    // const wordList = dummy.words.filter(word => 
+    //     word.day === Number(day))
+    // const [words, setWords] = useState([]);
 
+    // useEffect(() => {
+    //     fetch(`http://localhost:3001/words?day=${day}`).then(res => {
+    //         return res.json();
+    //     })
+    //     .then(data => {
+    //         setWords(data);
+    //     })
+    // }, [day]);
     return (
+        <>
+        <h2>Day {day}</h2>
         <table>
             <tbody>
-                {wordList.map(word => (
-                <tr>
-                    <td>{word.eng}</td>
-                    <td>{word.kor}</td>
-                </tr>
+                {words.map(word => (
+                    <Word word={word} key={word.id}/>
                 ))}
             </tbody>
         </table>
+        </>
     )
 }
